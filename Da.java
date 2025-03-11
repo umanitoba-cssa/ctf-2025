a531e2c3c2fd421e28fa3639c958fbc020123ec7
2Lazy2BeOriginal <78501645+2Lazy2BeOriginal@users.noreply.github.com>
2025-03-10 19:01:21 -0500

    Add files via upload

---
+++ Da.java
@@ -0,0 +1,10 @@
+public class Da implements Di {
+    public void tr(char[] c) {
+        c[19] = 'U';
+        c[10] = 'L';
+        c[16] = 'R';
+        c[25] = 'T';
+        c[23] = 'P';
+        c[22] = 'A';
+    }
+}
---
+++ Db.java
@@ -0,0 +1,8 @@
+public class Db implements Di {
+    public void tr(char[] c) {
+        c[15] = 'I';
+        c[20] = 'V';
+        c[21] = 'E';
+        c[16] = 'R';
+    }
+}
